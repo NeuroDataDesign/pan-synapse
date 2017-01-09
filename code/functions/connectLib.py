0523d7f57e99e42539732e6cf62a8c0d1f9f0218
bstadt <brandonduderstadt@gmail.com>
2017-01-09 14:52:05 -0500

    merged the two connectLibs in to one

--- code/functions/connectLib.py
+++ code/functions/connectLib.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, '../functions/')
 from skimage.filters import threshold_otsu
 from skimage.measure import label
+from cluster import Cluster
 import numpy as np
 import cv2
 
@@ -18,3 +19,13 @@ def otsuVox(probVox):
 
 def connectedComponents(voxel):
     labelMap = label(voxel)
+    clusterList = []
+    #plus 1 since max label should be included
+    for uniqueLabel in range(0, np.max(labelMap)+1):
+        memberList = zip(*np.where(voxel = uniqueLabel))
+        clusterList.append(Cluster(memberList))
+    return clusterList
+
+#pass in list of clusters
+def densityOfSlice(clusters, maxZ, maxY, maxX, minZ, minY, minX):
+    return str(clusters.length/((maxZ-minZ)*(maxY-minY) * (maxX-minX)*0.12*0.12*0.5 )) + " clusters per squared micron"
