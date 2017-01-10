e0c1d2de00250c6214a21594dee0cb98c3756a4c
Will LeVine <levinwil2@gmail.com>
2017-01-09 22:59:42 -0500

    changed get compactness method to measure standard deviation of distances from centroid

--- code/functions/cluster.py
+++ code/functions/cluster.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Cluster:
     def __init__(self, members):
@@ -13,13 +14,14 @@ class Cluster:
         listX = unzipList[2]
         return [np.average(listZ), np.average(listY), np.average(listX)]
 
-    #TODO develop a better compactness metric
     def getCompactness(self):
         unzipList = zip(*self.members)
         listZ = unzipList[0]
         listY = unzipList[1]
         listX = unzipList[2]
-        z_size_of_cluster = max(listZ) - min(listZ)
-        y_size_of_cluster = max(listY) - min(listY)
-        x_size_of_cluster = max(listX) - min(listX)
-        return len(self.members)/(z_size_of_cluster*y_size_of_cluster*x_size_of_cluster)
+        listOfDistances = []
+        volume = ((max(listZ) - min(listZ))*(max(listY) - min(listY))*(max(listX) - min(listX)))
+        for location in self.members:
+            listOfDistances.append(math.sqrt((location[0]-self.centroid[0])**2 + (location[1]-self.centroid[1])**2 + (location[2]-self.centroid[2])**2))
+        stdDevDistance = np.std(listOfDistances)
+        return stdDevDistance/volume
--- code/tests/clusterTests.py
+++ code/tests/clusterTests.py
@@ -2,14 +2,21 @@ import sys
 sys.path.insert(0, '../functions/')
 from epsilonDifference import epsilonDifference as floatEq
 from cluster import Cluster
+import epsilonDifference as epDiff
 
 testData1 = [[3,3,3], [3,3,2], [3,3,4], [3,2,3], [3,4,3], [2,3,3], [4,3,3]]
 testData2 = [[3,3,3], [3,3,4], [3,4,4], [3,4,5], [3,5,5], [4,5,5], [4,5,6]]
+testData3 = [[0, 0, 0], [0, 0, 1], [1, 0, 0], [0, 1, 0], [1, 1, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1],]
 
 print 'Cluster in cluster.py'
 testCluster1 = Cluster(testData1)
 testCluster2 = Cluster(testData2)
+testCluster3 = Cluster(testData3)
 
 #test the centroid method
-test1 = testCluster1.getCentroid()
-print '\tTest 1: ', test1 == [3., 3., 3.],'\n\t\tExpected: [3, 3, 3]\tResult: ', test1
+print '\tTest 1: ', testCluster1.getCentroid() == [3., 3., 3.],'\n\t\tExpected: [3, 3, 3]\tResult: ', testCluster1.getCentroid()
+print '\tTest 2: ', epDiff.epsilonDifference(3.28571429, testCluster2.getCentroid()[0], .001) and epDiff.epsilonDifference(4.14285714, testCluster2.getCentroid()[1], .001) and epDiff.epsilonDifference(4.57142857, testCluster2.getCentroid()[2], .001),'\n\t\tExpected: [3.28571429, 4.14285714, 4.57142857]\tResult: ', testCluster2.getCentroid()
+print '\tTest 3: ', testCluster3.getCentroid() == [0.5, 0.5, 0.5],'\n\t\tExpected: [0.5, 0.5, 0.5]\tResult: ', testCluster3.getCentroid()
+
+#test the compactness method
+print '\tTest 4: ', testCluster3.getCompactness() == 0,'\n\t\tExpected: 0\tResult: ', testCluster3.getCompactness()
