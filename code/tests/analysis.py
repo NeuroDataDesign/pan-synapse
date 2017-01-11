ae046871405c55faf4a9569895dc266e4d4fdc40
Richard Guo <richardg7890@gmail.com>
2017-01-11 14:25:45 -0500

    matplotlib versions of mouseVis

--- code/tests/analysis.py
+++ code/tests/analysis.py
@@ -11,15 +11,13 @@ import numpy as np
 import cv2
 
 #load the data
-data0 = tIO.unzipChannels(tIO.loadTiff('../../data/SEP-GluA1-KI_tp1.tif'))[0][0:1]
+data0 = tIO.unzipChannels(tIO.loadTiff('../../data/SEP-GluA1-KI_tp1.tif'))[0][0:5]
 
 #generate a foreground probability map for the data
 probVox = pLib.pipeline(data0)
 
 #generate a histogram to show bimodlity of the foreground probs
-mv.generateVoxHist(probVox)
-plt.show()
-
+mv.generateVoxHistogram(probVox)
 
 #get the otsu binarization of the supervoxel
 bianVox = cLib.otsuVox(probVox)
