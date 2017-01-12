4f1a6428f482fef7fcae7f8c541e6df2ba2b55a7
Will LeVine <levinwil2@gmail.com>
2017-01-12 14:52:18 -0500

    added to SLIC algorithm.md file

--- code/tests/synthDat/genData.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pickle
-
-array = np.zeros((100, 100))
-for i in range(20):
-    for j in range(20):
-        array[5*(2*j): 5*(2*j + 1), 5*(2*i): 5*(2*i + 1)] = 100
-pickle.dump(array, open( "clusterGrid.synth", "w" ) )
