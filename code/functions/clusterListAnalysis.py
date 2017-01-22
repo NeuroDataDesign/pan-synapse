efd59ba6272b350a65524c73400f0b4e4b3dd7cd
Will LeVine <levinwil2@gmail.com>
2017-01-22 18:35:43 -0500

    new method for thresholding raw data - binary

---
+++ code/functions/clusterListAnalysis.py
@@ -0,0 +1,29 @@
+import pickle
+import cluster
+import numpy as np
+import matplotlib.pyplot as plt
+
+volumeList = []
+clusters = pickle.load(open('afterPipeline.cluster'))
+print 'volumes: '
+for cluster in range(len(clusters)):
+    volumeList.append(clusters[cluster].getVolume())
+    print clusters[cluster].getVolume()
+
+print 'length: '
+print len(volumeList)
+print 'average: ' + str(np.average(volumeList))
+print 'percentage of data: ' + str(np.sum(volumeList)*1.0/(1024*1024*4))
+
+print "displaying as picture: "
+indexList = []
+for cluster in range(len(clusters)):
+    indexList.extend(clusters[cluster].members)
+
+synapseImage = np.zeros((5, 1024, 1024))
+for index in range(len(indexList)):
+    z, y, x = indexList[index]
+    synapseImage[z][y][x] = 100
+
+plt.imshow(synapseImage[2], cmap = 'gray')
+plt.show()
--- code/functions/connectLib.py
+++ code/functions/connectLib.py
@@ -6,6 +6,7 @@ from cluster import Cluster
 import numpy as np
 import cv2
 import plosLib as pLib
+import pickle
 
 def otsuVox(argVox):
     probVox = np.nan_to_num(argVox)
@@ -44,56 +45,83 @@ def densityOfSlice(clusters, minZ, maxZ, minY, maxY, minX, maxX):
     return clusterPerPixelCubed/(.12*.12*.5)
 
 #pass in list of clusters, return a list of thresholded clusters
-def naiveThreshold(clusterList):
-    #putting the finalclusters volumes in a list
-    finalClusterList =[]
-    for cluster in clusterList:
-        if cluster.getVolume() > 3 and cluster.getVolume() < 104:
-            finalClusterList.append(cluster)
+def thresholdByVolumePercentile(clusterList):
+    #putting the plosPipeline clusters volumes in a list
+    plosClusterVolList =[]
+    for cluster in (range(len(clusterList))):
+        plosClusterVolList.append(clusterList[cluster].getVolume())
 
     #finding the upper outlier fence
-    '''
-    Q3 = np.percentile(finalClusterVolList, 75)
-    Q1 = np.percentile(finalClusterVolList, 25)
+    Q3 = np.percentile(plosClusterVolList, 75)
+    Q1 = np.percentile(plosClusterVolList, 25)
     IQR = Q3 - Q1
     upperThreshFence = Q3 + 1.5*IQR
+    lowerThreshFence = Q1 - 1.5*IQR
 
     #filtering out the background cluster
     upperThreshClusterList = []
     for cluster in (range(len(clusterList))):
         if clusterList[cluster].getVolume() < upperThreshFence:
             upperThreshClusterList.append(clusterList[cluster])
-            '''
-    return finalClusterList
 
-def getClusterVolumes(clusterList):
-    clusterVols = []
-    for cluster in clusterList:
-        clusterVols.append(cluster.getVolume())
-    return clusterVols
+    return upperThreshClusterList
 
-def thresholdBackground(plosClusterList):
-    plosClusterVolumes = getClusterVolumes(plosClusterList)
-    #finding the upper outlier fence
-    Q3 = np.percentile(plosClusterVolumes, 75)
-    Q1 = np.percentile(plosClusterVolumes, 25)
-    IQR = Q3 - Q1
-    upperThreshFence = Q3 + 1.5*IQR
-    finalPlosList = []
+#pass in list of clusters, return a list of thresholded clusters
+def thresholdByVolumeNaive(clusterList):
+
     #filtering out the background cluster
-    for i in range(len(plosClusterList)):
-        if plosClusterList[i].getVolume() < upperThreshFence:
-            finalPlosList.append(plosClusterList[i])
+    naiveThreshClusterList = []
+    for cluster in (range(len(clusterList))):
+        if clusterList[cluster].getVolume() < 200:
+            naiveThreshClusterList.append(clusterList[cluster])
 
-    return finalPlosList
+    return naiveThreshClusterList
 
 #pass in the list of clusters that have gone through the plos pipeline, and the list that hasn't
 def clusterCoregister(plosClusterList, rawClusterList):
+    #creating a list of all the member indices of the plos cluster list
+    plosClusterMemberList = []
+    for cluster in range(len(plosClusterList)):
+        plosClusterMemberList.extend(plosClusterList[cluster].members)
+
     #creating a list of all the clusters without any decay
     finalClusterList =[]
-    for rawCluster in rawClusterList:
-        for cluster in plosClusterList:
-            if any(member in rawCluster.getMembers() for member in cluster.getMembers()):
-                finalClusterList.append(rawCluster)
+    for rawCluster in range(len(rawClusterList)):
+        if any(i for i in rawClusterList[rawCluster].members if i in plosClusterMemberList) and not(rawClusterList[rawCluster] in finalClusterList):
+            finalClusterList.append(rawClusterList[rawCluster])
 
     return finalClusterList
+
+def adaptiveThreshold(img):
+    img = (img/256).astype('uint8')
+    threshImg = np.zeros_like(img)
+    for i in range(len(img)):
+        threshImg[i] = cv2.adaptiveThreshold(img[i], 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=61, C=6)
+    return threshImg
+
+def binaryThreshold(img):
+    img = (img/256).astype('uint8')
+    threshImg = np.zeros_like(img)
+    percentile = np.percentile(img, 90)
+    for i in range(len(img)):
+        threshImg[i] = cv2.threshold(img[i], percentile, 255, cv2.THRESH_BINARY)[1]
+    return threshImg
+
+def completePipeline(combinedIm):
+    #finding the clusters after plosPipeline - list the decayed clusters
+    plosOut = pLib.pipeline(combinedIm)
+    bianOut = otsuVox(plosOut)
+    connectList = connectedComponents(bianOut)
+
+    threshClusterList = thresholdByVolumeNaive(connectList)
+    pickle.dump(threshClusterList, open('plosListAfterThresh.cluster', 'w'))
+
+    #finding the clusters without plosPipeline - lists the entire clusters
+    bianRawOut = binaryThreshold(combinedIm)
+    clusterRawList = connectedComponents(bianRawOut)
+    clusterRawThreshList = thresholdByVolumeNaive(clusterRawList)
+    pickle.dump(clusterRawThreshList, open('rawList.cluster', 'w'))
+
+    completeClusterMemberList = clusterCoregister(threshClusterList, clusterRawThreshList)
+
+    return completeClusterMemberList
