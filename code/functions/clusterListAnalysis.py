a805e26bad90386583f545e83aa6e7a452726f1b
Will LeVine <levinwil2@gmail.com>
2017-01-23 15:39:52 -0500

    implimented plotly methods

--- code/functions/clusterListAnalysis.py
+++ code/functions/clusterListAnalysis.py
@@ -2,28 +2,14 @@ import pickle
 import cluster
 import numpy as np
 import matplotlib.pyplot as plt
+import mouseVis as mv
 
-volumeList = []
 clusters = pickle.load(open('complete.clusters'))
-print 'volumes: '
+volumeList = np.zeros((len(clusters)))
 for cluster in range(len(clusters)):
-    volumeList.append(clusters[cluster].getVolume())
-    print clusters[cluster].getVolume()
+    volumeList[cluster] = clusters[cluster].getVolume()
 
-print 'length: '
-print len(volumeList)
-print 'average: ' + str(np.average(volumeList))
+print 'number of clusters: ' + str(len(volumeList))
+print 'average volume: ' + str(np.average(volumeList))
 print 'percentage of data: ' + str(np.sum(volumeList)*1.0/(1024*1024*4))
-
-print "displaying as picture: "
-indexList = []
-for cluster in range(len(clusters)):
-    indexList.extend(clusters[cluster].members)
-
-synapseImage = np.zeros((5, 1024, 1024))
-for index in range(len(indexList)):
-    z, y, x = indexList[index]
-    synapseImage[z][y][x] = 100
-print synapseImage[2]
-plt.imshow(synapseImage[2], cmap = 'gray')
-plt.show()
+mv.generateHist(volumeList, normed = False, bins = 100, xaxis = 'Volume', yaxis = 'frequency', figName='Cluster Volumes')
--- code/functions/mouseVis.py
+++ code/functions/mouseVis.py
@@ -1,63 +1,30 @@
 import tiffIO as tIO
-import plotly.plotly as py
 import plotly.graph_objs as go
+import plotly.offline as py
 import numpy as np
 import matplotlib.pyplot as plt
 import cPickle as pickle
-py.sign_in('levinwil', 'zs8MPogNdFIeRkMXi62h')
 
-#matplotlib method for histogram
-def generateHist(voxel, title = 'Untitled', bins = 50, xaxis = 'untitled_axis', yaxis = 'untitled_axis', axisStart = None, axisEnd = None, normed = True, pickle = False):
-    voxel = np.array(voxel)
-    voxel = voxel.flatten()
-    if (type(axisStart) == type(1) or type(axisStart) == type(1.0)):
-        start = axisStart
-    else:
-        start = np.min(voxel)
-    if (type(axisEnd) == type(1) or type(axisEnd) == type(1.0)):
-        end = axisEnd
-    else:
-        end = np.max(voxel)
-    plot = plt.hist(voxel, bins= bins,  range = (start, end), normed= normed)
-    if (pickle):
-        pickle.dump(plot, open(title + '.histogram', 'w'))
-    plt.title(title)
-    plt.xlabel(xaxis)
-    plt.ylabel(yaxis)
-    plt.show()
 
-#matplotlib method for plotting mutliple histograms
-def generateMultiHist(voxelList, title = 'Untitled', bins=10, xaxis = 'untitled_axis', yaxis = 'untitled_axis', axisStart = None, axisEnd = None, normed = True, stacked = True, pickle = False):
-    for i in range(len(voxelList)):
-        voxelList[i] = np.array(voxelList[i])
-        voxelList[i] = voxelList[i].flatten()
-    if (type(axisStart) == type(1) or type(axisStart) == type(1.0)):
-        start = axisStart
-    else:
-        start = np.min(voxel)
-    if (type(axisEnd) == type(1) or type(axisEnd) == type(1.0)):
-        end = axisEnd
-    else:
-        end = np.max(voxel)
-    plt.hist(voxelList, bins = bins, range = (start, end), normed = normed, stacked = stacked)
-    plt.title(title)
-    plt.xlabel(xaxis)
-    plt.ylabel(yaxis)
-    plt.show()
-    
-#plotly method for histograms
-'''
-def generateVoxHist(voxel, figName='untitled', figNum=-1, bins=10, axisStart=None, axisEnd=None, normed=False, xaxis='untitled_axis', yaxis='untitled_axis'):
+def generateHist(voxel, figName='untitled', figNum=-1, bins=10, axisStart=None, axisEnd=None, normed=False, xaxis='untitled_axis', yaxis='untitled_axis'):
     for i in range(len(voxel.shape)-1):
         voxel = voxel.flatten()
-    print voxel.shape
+    if normed == False:
+        norm=''
+    else:
+        norm='probability'
+    if axisStart == None:
+        axisStart = np.min(voxel)
+    if axisEnd == None:
+        axisEnd = np.max(voxel)
     data = [
         go.Histogram(
             x = voxel,
-            histnorm='probability',
+            histnorm=norm,
             xbins=dict(
                 start = axisStart,
-                end = axisEnd
+                end = axisEnd,
+                size = (axisEnd - axisStart)/bins
             )
         )
     ]
@@ -72,23 +39,33 @@ def generateVoxHist(voxel, figName='untitled', figNum=-1, bins=10, axisStart=Non
     )
     fig = go.Figure(data=data, layout=layout)
     py.plot(fig)
-'''
+
 
 
 #plotly method for generating stacked histograms
-'''
-def generateMultiVoxHist(voxelList, figName='untitled', figNum=None, bins=10, axisStart=None, axisEnd=None, normed=False, xaxis='untitled_axis', yaxis='untitled_axis'):
-    data =[]
+
+def generateMultiHist(voxelList, figName='untitled', figNum=None, bins=10, axisStart=None, axisEnd=None, normed=False, xaxis='untitled_axis', yaxis='untitled_axis'):
+    for voxel in range(len(voxelList)):
+        for i in range(len(voxelList[voxel].shape)-1):
+            voxelList[voxel] = voxelList[voxel].flatten()
+    if normed == False:
+        norm=''
+    else:
+        norm='probability'
+    if axisStart == None:
+        axisStart = np.min(voxelList)
+    if axisEnd == None:
+        axisEnd = np.max(voxelList)
+    data = []
     for i in range(len(voxelList)):
         data.append(go.Histogram(
                         x = voxelList[i],
-                        histnorm='probability',
+                        histnorm=norm,
                         xbins=dict(
                             start = axisStart,
                             end = axisEnd,
-                            size = 0.7
-                        )
-                    ))
+                            size = (axisEnd - axisStart)/bins
+                    )))
     layout = go.Layout(
         title=figName,
         xaxis=dict(
@@ -101,4 +78,3 @@ def generateMultiVoxHist(voxelList, figName='untitled', figNum=None, bins=10, ax
     )
     fig = go.Figure(data=data, layout=layout)
     py.plot(fig)
-'''
