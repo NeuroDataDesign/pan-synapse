87f57a3c5c7f672c826fbcf9d2662d6992298fe4
Richard Guo <richardg7890@gmail.com>
2017-01-27 16:13:42 -0500

    fixed html forms

--- code/service/server/server.py
+++ code/service/server/server.py
@@ -2,10 +2,18 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+    if request.method == 'POST':
+        print "I got it"
+        return redirect('http://localhost:5000/analyze')
+
+@app.route('/analyze', methods = ['GET', 'POST'])
+def analyze():
+    if request.method == 'GET':
+        return render_template('brain.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
