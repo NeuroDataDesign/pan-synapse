9e3dcea3c30263787cfd6f23dfbfec883e992d58
bstadt <brandonduderstadt@gmail.com>
2017-04-10 14:39:13 -0400

    updated output

--- code/functions/runPipeline.py
+++ code/functions/runPipeline.py
@@ -7,7 +7,7 @@ import os
 import csv
 from cluster import Cluster
 
-def generateCSV(myID, results):
+def generateCSV(results):
     myOutput = []
     for elem in results:
         centroid1 = elem.getCentroid()
@@ -15,21 +15,23 @@ def generateCSV(myID, results):
         myOutput.append([centroid1[0], centroid1[1], centroid1[2], elem.getVolume(),
                          centroid2[0], centroid2[1], centroid2[2], elem.timeRegistration.getVolume()])
 
-    with open('../service/static/results/' + str(myID)+'.csv','w') as outFile:
+    with open('../../results','w') as outFile:
         scribe = csv.writer(outFile)
         scribe.writerows(myOutput)
+
+    print 'pipeline complete'
 
 
 #This library is designed to act as the driver for the docker and the web service
 def runPipeline(myID):
     if len(glob.glob('../../data/*.tif')) != 0:
-        fileList =  glob.glob('../service/static/data/' + str(myID) + '_*.tif')
+        fileList =  sorted(glob.glob('../../data/*.tif'))
     else:
         sys.exit("FILE NOT FOUND")
 
     #Generate results form pipeline
     print 'Starting Pipeline'
-    results = pipe.pipeline(fileList, verbose=True)
+    results = pipe.pipeline(fileList[0], fileList[1], verbose=False)
 
     #generate visualization
     #mv.generatePlotlyLineGraph(myID, results)
