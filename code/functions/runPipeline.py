333b10f682004dc1a47f632adfa46d87c558a9fa
bstadt <brandonduderstadt@gmail.com>
2017-04-10 12:57:34 -0400

    added csv genreation for demo

--- code/functions/runPipeline.py
+++ code/functions/runPipeline.py
@@ -5,12 +5,19 @@ import cv2
 import glob
 import os
 import csv
+from cluster import Cluster
 
 def generateCSV(myID, results):
-    #results is a dictionary of n-tuples where n is a timepoint
+    myOutput = []
+    for elem in results:
+        centroid1 = elem.getCentroid()
+        centroid2 = elem.timeRegistration.getCentroid()
+        myOutput.append([centroid1[0], centroid1[1], centroid1[2], elem.getVolume(),
+                         centroid2[0], centroid2[1], centroid2[2], elem.timeRegistration.getVolume()])
+
     with open('../service/static/results/' + str(myID)+'.csv','w') as outFile:
         scribe = csv.writer(outFile)
-        scribe.writerows(results)
+        scribe.writerows(myOutput)
 
 
 #This library is designed to act as the driver for the docker and the web service
@@ -25,7 +32,7 @@ def runPipeline(myID):
     results = pipe.pipeline(fileList, verbose=True)
 
     #generate visualization
-    mv.generatePlotlyLineGraph(myID, results)
+    #mv.generatePlotlyLineGraph(myID, results)
 
     #generate csv
     generateCSV(myID, results)
