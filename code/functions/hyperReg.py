8d73f8807bf4a0fe227ce36ab9a3846991e71fe4
bstadt <brandonduderstadt@gmail.com>
2017-04-27 16:20:33 -0400

    added deliverables

--- code/functions/hyperReg.py
+++ code/functions/hyperReg.py
@@ -7,41 +7,19 @@ from scipy.ndimage.measurements import center_of_mass
 from random import randrange as rand
 from random import uniform as floatRand
 
-def get3DRigid(pitchRange = .1,
-               rollRange = .1,
-               yawRange = .1,
-               xRange = 0.,
-               yRange = 0.,
-               zRange = 0.,
-               pitch=None,
-               yaw=None,
-               roll=None,
-               zT=None,
-               yT=None,
-               xT=None):
+def get3DRigid(roll=0,
+               pitch=0,
+               yaw=0,
+               zT=0,
+               yT=0,
+               xT=0):
 
-    #generate a random rigid body transform in given bounds
-    a = floatRand(pitchRange-(pitchRange/2.), pitchRange+(pitchRange/2.))
-    b = floatRand(yawRange-(yawRange/2.), yawRange+(yawRange/2.))
-    c = floatRand(rollRange-(rollRange/2.), rollRange+(rollRange/2.))
-
-    xt = floatRand(xRange-(xRange/2.), xRange+(xRange/2.))
-    yt = floatRand(yRange-(yRange/2.), yRange+(yRange/2.))
-    zt = floatRand(zRange-(zRange/2.), zRange+(zRange/2.))
-
-    #set default params, if passed
-    if yaw is not None:
-        a = yaw
-    if pitch is not None:
-        b = pitch
-    if roll is not None:
-        c = roll
-    if xT is not None:
-        xt = xT
-    if yT is not None:
-        yt = yT
-    if zT is not None:
-        zt = zT
+    a = yaw
+    b = pitch
+    c = roll
+    xt = xT
+    yt = yT
+    zt = zT
 
     #generate the transform
     transform = np.stack([
@@ -52,7 +30,8 @@ def get3DRigid(pitchRange = .1,
     ])
     return transform
 
-def apply3DRigid(initialVolume, transform, verbose=False):
+
+def apply3DRigidVolume(initialVolume, transform, originZ, originY, originX, verbose=False):
     #create a body to hold return values
     rigidMatrix = np.zeros_like(initialVolume)
 
@@ -62,11 +41,12 @@ def apply3DRigid(initialVolume, transform, verbose=False):
 
     #convert every voxel
     for z in range(initialVolume.shape[0]):
+        print z/float(initialVolume.shape[0])
         for y in range(initialVolume.shape[1]):
             for x in range(initialVolume.shape[2]):
 
                 #shift the volume so that the transform is applied to the center, and not the top left back edge
-                shift = [initialVolume.shape[2]/2., initialVolume.shape[1]/2., initialVolume.shape[0]/2.]
+                shift = [originX, originY, originZ]
                 shiftedPoint = list(np.subtract([x, y, z], shift))
                 shiftedPoint.append(1.)
 
@@ -93,63 +73,67 @@ def apply3DRigid(initialVolume, transform, verbose=False):
 
     return rigidMatrix
 
-def align3DCOM(npVolA, npVolB, verbose=False):
-    #get the centers of mass
-    comA = center_of_mass(npVolA)
-    comB = center_of_mass(npVolB)
+def apply3DRigid(pointList, transform, originZ, originY, originX, verbose=False):
+    #create var to keep track of out of bounds percent
+    out = 0.
+    sigOut = 0.
 
-    if verbose:
-        print 'comA', comA, '\t', 'comB: ', comB
+    #convert every voxel
+    for voxel in pointList:
 
-    #find the disperity
-    translationVector = np.subtract(comB, comA)
+        z = voxel[0]
+        y = voxel[1]
+        x = voxel[2]
 
-    #prep the vectorfor apply3DRigid
-    translationVector = np.flipud(translationVector)
-    translationVector = np.append(translationVector, 1.)
-    transform = np.identity(4)
-    transform[:,3]=translationVector
+        #shift the volume so that the transform is applied to the center, and not the top left back edge
+        shift = [originX, originY, originZ]
+        shiftedPoint = list(np.subtract([x, y, z], shift))
+        shiftedPoint.append(1.)
 
-    if verbose:
-        print 'transform: ', transform
+        #calculate the transform, drop the irrelavent 1 at the end
+        new = np.dot(transform, shiftedPoint)[:3]
 
-    return apply3DRigid(npVolA, transform, verbose=verbose)
+        #shift the volume back to its original spot after transform
+        final = np.add(new, shift)
+        return list(reversed(final))
 
-def simpleRegister(clusters1, clusters2, verbose=False):
-    pairings = []
-    this = len(clusters1)
-    for clusterIdx in range(len(clusters1)):
-        if verbose:
-            print 'Progress: ', clusterIdx/float(this)
-        lossList = [np.sum(abs(np.array(clusters1[clusterIdx].centroid) - np.array(elem.centroid))) for elem in clusters2]
-        pairings.append((clusterIdx, np.argmin(lossList)))
-    return pairings
+def parseTransformFile(filePath):
+    with open(filePath, 'r') as transformFile:
+        content = transformFile.readlines()
 
-def randomSubset(original, k):
-    subset = []
-    while len(subset) != k:
-        chosen = original[rand(0, len(original))]
-        if not chosen in subset:
-            subset.append(chosen)
-    return subset
+    #strip whitespace
+    content = [line.strip() for line in content]
 
-def resolve(regList, toTrack = 50):
+    originZ, originY, originX, = None, None, None
+    roll, pitch, yaw = None, None, None
+    zT, yT, xT = None, None, None
 
-    #NOTE as of current this selects a random subset of 50 nodes to trace
-    print 'here'
-    toTrack = regList[0][:toTrack]
-    #initialize a container for the final sequence
-    seq = []
+    for line in content:
+        if 'CenterOfRotationPoint' in line:
+            params = line.split()
+            originZ = float(params[1])
+            originY = float(params[2])
+            #the comprehension at the end is to remove the trailing )
+            originX = float(params[3][:-1])
 
-    #Follow each pairing like a thread
-    for num, elem in enumerate(toTrack):
-        print 'progress: ', num/50.
-        print elem
-        #initialize a container to hold the current thread
-        myThread = [elem[0]]
-        #starts at 1 since 0 seed is already in thread
-        for tp in range(1, len(regList)):
-            myThread.append(regLst[tp-1][myThread[-1]][1])
-        seq.append(myThread)
+        #The leading ( is there to make sure that 'InitialTransformParameters is not grabbed
+        if '(TransformParameters' in line:
+            params = line.split()
+            roll = float(params[1])
+            pitch = float(params[2])
+            yaw = float(params[3])
+            zT = float(params[4])
+            yT = float(params[5])
+            #the comprehension at the end is to remove the trailing )
+            xT = float(params[6][:-1])
 
-    return seq
+    matrix = get3DRigid(
+        roll=roll,
+        pitch=pitch,
+        yaw=yaw,
+        zT=zT,
+        yT=yT,
+        xT=xT
+    )
+
+    return {'originZ':originZ, 'originY':originY, 'originX':originX, 'matrix':matrix}
