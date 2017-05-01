1e0aba4bf590a89096c3753629bbdfaf2147aef5
Richard Guo <richardg7890@gmail.com>
2017-04-30 23:31:04 -0400

    robust cloud

--- code/cloud/synapsys_cloud.py
+++ code/cloud/synapsys_cloud.py
@@ -1,11 +1,21 @@
 import boto3
 import sys
+import argparse
 sys.path.insert(0, "../functions")
 
 def submitJob(name, bucket, path, tp1, tp2):
-    createComputeEnvironment()
-    createJobQueue()
-    createJobDefinition()
+    try:
+        createComputeEnvironment()
+    except:
+        print "Existing Compute Environment found"
+    try:
+        createJobQueue()
+    except:
+        print "Exisiting Job Queue found"
+    try:
+        createJobDefinition()
+    except:
+        print "Existing Job Definition found"
     response = client.submit_job(
         jobName = str(name),
         jobQueue = "synapsys_jobs",
@@ -29,10 +39,10 @@ def createJobDefinition():
             "command": ["python", "runPipeline.py"]
         },
     )
-    print "JOB DEF:\n" + response
+    print "JOB DEF:\n" + str(response)
     return "synapsys_pipeline"
 
-def createJobQueue(computeEnviron):
+def createJobQueue():
     response = client.create_job_queue(
         jobQueueName= "synapsys_jobs",
         state = "ENABLED",
@@ -40,11 +50,11 @@ def createJobQueue(computeEnviron):
         computeEnvironmentOrder = [
             {
                 "order": 1,
-                "computeEnvironment": computeEnviron
+                "computeEnvironment": "synapsys_ce"
             }
         ],
     )
-    print "JOB QUEUE:\n" + response
+    print "JOB QUEUE:\n" + str(response)
     return "synapsys_jobs"
 
 def createComputeEnvironment():
@@ -57,26 +67,33 @@ def createComputeEnvironment():
             "minvCpus": 0,
             "maxvCpus": 256,
             "desiredvCpus": 0,
-            "instanceTypes": ["t2.large"],
+            "instanceTypes": ["m4.large"],
+            "subnets": ["subnet-17c65f72", "subnet-75006549", "subnet-4e2ace06", "subnet-7ca59151", "subnet-74f3d02f"],
+            "securityGroupIds": ["sg-41927a3e"],
+            "instanceRole": "arn:aws:iam::389826612951:instance-profile/ecsInstanceRole",
         },
+        serviceRole='arn:aws:iam::389826612951:role/service-role/AWSBatchServiceRole'
     )
     print "COMPUTE ENV:\n" + response
     return "synapsys_ce"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Synapsys pipeline on the cloud')
+    parser.add_argument('name', type = str, help= 'Job name')
     parser.add_argument('bucket', type = str, help='The S3 bucket with the input dataset')
     parser.add_argument('path', type = str, help = 'The S3 folder with the input dataset')
-    parser.add_argument('timepoint1', type = str, help = 'The key to the first timepoint file. Key '+
+    parser.add_argument('tp1', type = str, help = 'The key to the first timepoint file. Key '+
                                         'should be same as filename. Must be formated as '+
                                         'uniqueID_1.tif. e.g. jhu_1.tif')
-    parser.add_argument('timepoint2', type = str, help = 'The key to the second timepoint file. Key '+
+    parser.add_argument('tp2', type = str, help = 'The key to the second timepoint file. Key '+
                                         'should be same as filename. Must be formated as '+
                                         'uniqueID_2.tif e.g. jhu_2.tif')
-    parser.add_argument('--AWS_SECRET_KEY', type = str)
+    #parser.add_argument('--AWS_SECRET_KEY', type = str, help = 'AWS Secret Keys')
     args = parser.parse_args()
+    name = args.name
     bucket = args.bucket
     path = args.path
-    tp1 = args.timepoint1
-    tp2 = args.timepoint2
-    submitJob(bucket, path, tp1, tp2)
+    tp1 = args.tp1
+    tp2 = args.tp2
+    client = boto3.client('batch')
+    submitJob(name, bucket, path, tp1, tp2)
