bf32d0f21b1638890b2e4d911d9ba92f3bc2678e
bstadt <brandonduderstadt@gmail.com>
2017-05-14 16:26:45 -0400

    added qa lib

---
+++ pipeline_3/code/functions/qaLib.py
@@ -0,0 +1,77 @@
+from cluster import Cluster
+
+def precision_recall_f1(labels, predictions):
+
+    if len(predictions) == 0:
+        print 'ERROR: prediction list is empty'
+        return 0., 0., 0.
+
+    labelFound = np.zeros(len(labels))
+    truePositives = 0
+    falsePositives = 0
+
+    for prediction in predictions:
+        #casting to set is ok here since members are uinque
+        predictedMembers = set([tuple(elem) for elem in prediction.getMembers()])
+        detectionCutoff = 1
+        found = False
+
+        for idx, label in enumerate(labels):
+            labelMembers = set([tuple(elem) for elem in label.getMembers()])
+            #if the predictedOverlap is over the detectionCutoff ratio
+            if len(predictedMembers & labelMembers) >= detectionCutoff:
+                truePositives +=1
+                found=True
+                labelFound[idx] = 1
+
+        if not found:
+            falsePositives +=1
+
+    precision = truePositives/float(truePositives + falsePositives)
+    recall = np.count_nonzero(labelFound)/float(len(labels))
+    f1 = 0
+    try:
+        f1 = 2 * (precision*recall)/(precision + recall)
+
+    except ZeroDivisionError:
+        f1 = 0
+
+    return precision, recall, f1
+
+def toDiff(imgA, imgB):
+    ret = np.empty((imgA.shape[0], imgA.shape[1], 3), dtype=np.uint8)
+    for y in range(imgA.shape[0]):
+        for x in range(imgA.shape[1]):
+
+            if imgA[y][x] and not imgB[y][x]:
+                ret[y][x][0] = 255
+                ret[y][x][1] = 0
+                ret[y][x][2] = 0
+
+            elif not imgA[y][x] and imgB[y][x]:
+                ret[y][x][0] = 0
+                ret[y][x][1] = 0
+                ret[y][x][2] = 255
+
+            elif imgA[y][x] and imgB[y][x]:
+                ret[y][x][0] = 0
+                ret[y][x][1] = 255
+                ret[y][x][2] = 0
+
+            else:
+                ret[y][x][0] = 255
+                ret[y][x][1] = 255
+                ret[y][x][2] = 255
+
+    return ret
+
+def visDiff(sliceA, sliceB):
+    disp = toDiff(sliceA, sliceB)
+    return disp
+
+def visVolDiff(volumeA, volumeB):
+    for i in range(volumeA.shape[0]):
+        plt.figure()
+        plt.title('Disperity at z=' + str(i))
+        plt.imshow(visDiff(volumeA[i], volumeB[i]))
+        plt.show()
