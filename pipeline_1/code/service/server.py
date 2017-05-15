b46d7e95e0b9c0a66034ad577198fde8602c1f42
Richard Guo <richardg7890@gmail.com>
2017-05-15 14:08:12 -0400

    some additional cloud functions, cloud demo done

--- pipeline_1/code/service/server.py
+++ pipeline_1/code/service/server.py
@@ -6,7 +6,7 @@ from flask import Flask, redirect, url_for, request, render_template, session, f
 
 app = Flask(__name__)
 
-def submitJob(name, path, tp1, tp2):
+def submitJob(bucket, name):
     client = boto3.client("batch")
     createComputeEnvironment()
     createJobQueue()
@@ -16,7 +16,7 @@ def submitJob(name, path, tp1, tp2):
         jobQueue = "synapsys_jobs",
         jobDefinition = "synapsys_pipeline",
         containerOverrides = {
-            "command": ["python", "runPipeline.py", path, tp1, tp2]
+            "command": ["python", "runPipeline_3.py", bucket, name]
         }
     )
 
@@ -29,28 +29,21 @@ def index():
 
 @app.route("/submitJob", methods = ["GET", "POST"])
 def submit():
-    tp1 = request.form["tp1"]
-    tp2 = request.form["tp2"]
     name = request.form["name"]
+    bucket = request.form["bucket"]
     errorName = False
-    errorTp1 = False
-    errorTp2 = False
-    if len(name) == 0  or len(tp1) == 0 or len(tp2) == 0:
+    if len(name) == 0  or len(bucket) == 0:
         if len(name) == 0:
             errorName = True
-        if len(tp1) == 0:
-            errorTp1 = True
-        if len(tp2) == 0:
-            errorTp2 = True
+        if len(bucket) == 0:
+            errorBucket = True
         return render_template("index.html/",
                                 errorName = errorName,
-                                errorTp1 = errorTp1,
-                                errorTp2 = errorTp2,
+                                errorBucket = errorBucket,
                                 name = name,
-                                tp1 = tp1,
-                                tp2 = tp2)
+                                bucket = bucket)
     else:
-        submitJob(name, tp1, tp2)
+        submitJob(bucket, name)
         return render_template("analyze.html")
 
 if __name__ == "__main__":
